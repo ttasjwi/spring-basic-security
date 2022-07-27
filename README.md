c8d22b570b46557b1bdefc6b0650630a992ca356
ttasjwi <ttasjwi920@gmail.com>
2022-07-27 16:12:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Core Spring Security
+- BasicSecurity : 기본 시큐리티 학습
+
+---
---
+++ src/main/java/com/ttasjwi/basicSecurity/controller/SecurityController.java
@@ -0,0 +1,16 @@
+package com.ttasjwi.basicSecurity.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        log.info("index!!!");
+        return "home";
+    }
+}
