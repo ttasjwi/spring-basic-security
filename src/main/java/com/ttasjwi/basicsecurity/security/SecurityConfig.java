10052a144377acc13f08200cc4132d536c0cf6e4
ttasjwi <ttasjwi920@gmail.com>
2022-12-13 14:37:54 +0900

    AnonymousAuthenticationFilter 실습
    - 디버거로 AnonymousAuthenticationFilter의 doFilter를 참조해볼 것
    - 익명 사용자의 Authentication을 null로 하지 않기 위함

--- src/main/java/com/ttasjwi/basicsecurity/security/SecurityConfig.java
+++ src/main/java/com/ttasjwi/basicsecurity/security/SecurityConfig.java
@@ -1,73 +1,25 @@
 package com.ttasjwi.basicsecurity.security;
 
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.http.HttpSession;
 
 
 @Slf4j
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(requests -> requests.anyRequest().authenticated());
-
-        http.formLogin();
-
-        http.logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .deleteCookies("JSESSIONID", "remember-me")
-                .addLogoutHandler(logoutHandler())
-                .logoutSuccessHandler(logoutSuccessHandler())
-            .and()
-                .rememberMe()
-                .rememberMeParameter("remember")
-                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService);
-    }
-
-    private static AuthenticationSuccessHandler loginSuccessHandler() {
-        return (request, response, authentication) -> {
-            log.info("authentication : {}", authentication.getName());
-            response.sendRedirect("/");
-        };
-    }
-
-    private static AuthenticationFailureHandler loginFailureHandler() {
-        return (request, response, exception) -> {
-            log.info("exception : {}", exception.getMessage());
-            response.sendRedirect("/login");
-        };
-    }
-
-    private LogoutHandler logoutHandler() {
-        return ((request, response, authentication) -> {
-            HttpSession session = request.getSession();
-            session.invalidate(); // 세션 무효화
-        });
-    }
-
-    private LogoutSuccessHandler logoutSuccessHandler() {
-        return ((request, response, authentication) -> {
-            response.sendRedirect("/login");
-        });
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated();
+        http
+                .formLogin();
     }
 
 }
